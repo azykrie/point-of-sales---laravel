2e86c713fb5ecf3153a3b2c370c788249c14eee9
Dhikri Haikal <dhikrihaikal1@gmail>
2025-11-04 06:24:56 +0700

    update v

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            "name"=> "Dhikri Haikal",
+            "email"=> "dhikrihaikal1@gmail.com",
+            "password"=> bcrypt("adikdikri12345"),
+            "role"=> "admin",
         ]);
     }
 }
